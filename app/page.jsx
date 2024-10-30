5d231d9943dd6dee7da7c45df3a86ce619b47c2b
David Udoh <udohdavid004@gmail.com>
2024-10-30 10:26:41 +0100

    launch

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,10 @@
 'use client';
 
 import React from 'react';
-import Landing from '../components/Landing';
+// import Landing from '../components/Landing';
 import MainPage from '../components/MainPage';
-import { useUser } from '@auth0/nextjs-auth0/client';
+// import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function Index() {
-  const { user } = useUser();
-  return <>{!user ? <Landing /> : <MainPage />}</>;
+  <MainPage />;
 }
